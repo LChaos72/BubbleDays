7b7bda403769296209b496e93e413d64498de78f
LChaos72 <71891706+LChaos72@users.noreply.github.com>
2020-10-02 00:58:46 +0530

    added game facility

---
+++ 2.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  /*long long t,m,i,n,q,a[1000000000],d=0;
+  cin>>t;
+
+  for(int j=0;j<t;i++)
+  {
+    cin>>m;
+    for(int k=0;k<m;k++)
+    { cin>>i>>n>>q;
+      if(n%2==0) a[d++]=n/2;
+      else
+      {
+        if(q==i)a[d++]=n/2;
+        else a[d++]=(n/2)+1;
+      }
+    }
+    }
+  for(int j=0;j<d;j++)
+  cout<<a[j]<<endl;*/
+  char b[1];
+  b[0]=char(65+2);
+  cout<<b[0];
+  cout<<int(b[0]);
+}
+//if n is even then 1=2=n/2
+//if n is odd then if q=i then output n/2 if not then output n/2 + 1
